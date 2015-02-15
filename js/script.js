54ea581ecfc949cdf2ad37eb729c2d7207c23a50
Ed Wu <edwardwu0@gmail.com>
2015-02-15 17:36:02 -0600

    changed navbar to be more robust

--- js/script.js
+++ js/script.js
@@ -1,5 +1,40 @@
 $(document).ready(function() {
-    $("p").click(function() {
-        $(this).hide();
+    //Store original height of navBar
+    var navBar = $('nav.main');
+    jQuery.data(navBar, 'oHeight', navBar.height());
+
+    $('ul#menu-main li a').on('click', function(event) {
+
+        var target = $($(this).attr('href'));
+
+        if (target.length) {
+            var navBarHeight = jQuery.data(navBar, 'oHeight');
+            event.preventDefault();
+
+            $('body,html').animate({
+                scrollTop: target.offset().top - navBarHeight * 0.5
+            }, 500);
+        }
+    });
+
+    $(window).scroll(function() {
+        var position = $(this).scrollTop();
+        //console.log(position);
+        var navBarHeight = jQuery.data(navBar, 'oHeight');
+
+        console.log(navBarHeight);
+
+        /*if (position > navBarHeight) {
+            navBar.stop().animate({
+                height: navBarHeight * 0.5
+            }, 500);
+        } else*/
+        if (position <= navBarHeight * 0.5 && position >= 0) {
+            navBar.stop().height(navBarHeight - position);
+        } else if (position > navBarHeight ) {
+            navBar.stop().animate({
+                height: navBarHeight * 0.5
+            }, 500);
+        }
     });
 });
